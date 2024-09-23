6c02fe0bfd21e29649d15b4ecc1845ab68f02f2d
MERCURY <wanasart2@gmail.com>
2024-09-23 12:02:39 +0700

    update: 23/09/67

---
+++ src_8/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        RunArray runArray = new RunArray();
+        RunDoublyLinkedList runDoublyLinkedList = new RunDoublyLinkedList();
+
+        //runArray.running();
+        runDoublyLinkedList.running();
+    }
+}
